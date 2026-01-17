c0b141355ca7c6d2c44f3c6e1cc96463bcfaff87
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2026-01-17 10:15:22 -0500

    node-with-docker-k8s

---
+++ .devcontainer/node-with-docker-k8s/installMoreTools.sh
@@ -0,0 +1,11 @@
+
+mkdir install-more-tools
+cd install-more-tools
+
+KIND_VERSION=$(curl -sL https://api.github.com/repos/kubernetes-sigs/kind/releases/latest | jq -r .tag_name)
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+cd ..
+rm -rf install-more-tools
